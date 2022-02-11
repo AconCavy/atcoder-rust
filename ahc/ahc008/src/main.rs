3d0428a47fa80d97c5395f304ce72c2d21e8ab31
AconCavy <aconcavy@gmail.com>
2022-02-12 05:19:27 +0900

    Add ahc008

---
+++ ahc/ahc008/src/main.rs
@@ -0,0 +1,12 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+
+use itertools::Itertools;
+use proconio::*;
+use std::fmt::{Display, Formatter};
+use std::time::{Duration, Instant};
+
+#[fastout]
+fn main() {
+    println!("Hello AHC008");
+}
