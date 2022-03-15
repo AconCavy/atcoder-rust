d5778ab1d9f4815178087853478007c06579efe9
AconCavy <aconcavy@gmail.com>
2022-03-16 02:15:31 +0900

    Modify dsu

--- mylib/src/dsu.rs
+++ mylib/src/dsu.rs
@@ -53,10 +53,34 @@ impl Dsu {
     }
 
     fn groups(&mut self) -> Vec<Vec<usize>> {
-        let mut result: Vec<Vec<usize>> = vec![Vec::new(); self.len];
+        let mut leader_of = vec![0; self.len];
+        let mut group_size = vec![0; self.len];
         for i in 0..self.len {
-            result.get_mut(self.leader_of(i)).unwrap().push(i);
+            leader_of[i] = self.leader_of(i);
+            group_size[leader_of[i]] += 1;
+        }
+        let mut result: Vec<Vec<_>> = group_size
+            .into_iter()
+            .map(|c| Vec::with_capacity(c))
+            .collect();
+        for (i, v) in leader_of.into_iter().enumerate() {
+            result[v].push(i);
         }
         result.into_iter().filter(|x| !x.is_empty()).collect()
     }
+}
+
+#[test]
+fn dsu_test() {
+    let mut dsu = Dsu::new(4);
+    dsu.merge(0, 1);
+    assert!(dsu.same(0, 1));
+    dsu.merge(0, 2);
+    assert_eq!(dsu.size_of(0), 3);
+    assert!(dsu.same(0, 2));
+    assert!(dsu.same(1, 2));
+    assert!(!dsu.same(0, 3));
+    assert!(!dsu.same(1, 3));
+    assert!(!dsu.same(2, 3));
+    assert_eq!(dsu.groups(), vec![vec![0, 1, 2], vec![3]]);
 }
