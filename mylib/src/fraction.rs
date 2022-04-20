b37d3b553d557ce046e9f98f8e9baa57a6cf3560
AconCavy <aconcavy@gmail.com>
2022-04-21 08:08:56 +0900

    Modify Fraction

--- mylib/src/fraction.rs
+++ mylib/src/fraction.rs
@@ -1,35 +1,34 @@
 #![allow(dead_code)]
 
+use crate::util::gcd;
+use num_integer::Integer;
+
 #[derive(Debug, Copy, Clone, Eq, Hash)]
-pub struct Fraction {
-    y: i64,
-    x: i64,
+pub struct Fraction<T: Copy + Integer> {
+    y: T,
+    x: T,
 }
 
-impl Fraction {
-    pub fn new(y: i64, x: i64) -> Self {
-        fn gcd(a: i64, b: i64) -> i64 {
-            return if b == 0 { a } else { gcd(b, a % b) };
-        }
-
+impl<T: Copy + Integer> Fraction<T> {
+    pub fn new(y: T, x: T) -> Self {
         let g = gcd(y, x);
         Self { y: y / g, x: x / g }
     }
 }
 
-impl Ord for Fraction {
+impl<T: Copy + Integer> Ord for Fraction<T> {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         (self.y * other.x).cmp(&(self.x * other.y))
     }
 }
 
-impl PartialEq<Self> for Fraction {
+impl<T: Copy + Integer> PartialEq<Self> for Fraction<T> {
     fn eq(&self, other: &Self) -> bool {
         self.y == other.y && self.x == other.x
     }
 }
 
-impl PartialOrd<Self> for Fraction {
+impl<T: Copy + Integer> PartialOrd<Self> for Fraction<T> {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         Some(self.cmp(other))
     }
