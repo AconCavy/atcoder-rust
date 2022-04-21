eb5365bfddfcbdcaa15f638bd5dbb2c936aee188
AconCavy <aconcavy@gmail.com>
2022-04-22 03:37:15 +0900

    Modify annealing

--- mylib/src/annealing.rs
+++ mylib/src/annealing.rs
@@ -8,33 +8,33 @@ pub trait AnnealingState {
     fn modify(&self) -> Self;
 }
 
-pub struct AnnealingConfig {
+pub struct AnnealingSimulator {
     time_limit_millis: u64,
     start_temp: f64,
     end_temp: f64,
     random_seed: u64,
 }
 
-pub struct AnnealingSimulator {
-    config: AnnealingConfig,
-}
-
 impl AnnealingSimulator {
-    pub fn new(config: AnnealingConfig) -> Self {
-        Self { config }
+    pub fn new(time_limit_millis: u64, start_temp: f64, end_temp: f64, random_seed: u64) -> Self {
+        Self {
+            time_limit_millis,
+            start_temp,
+            end_temp,
+            random_seed,
+        }
     }
 
     pub fn simulate<S: AnnealingState>(&self, init_state: S) -> S {
         let timer = Instant::now();
-        let end = Duration::from_millis(self.config.time_limit_millis);
+        let end = Duration::from_millis(self.time_limit_millis);
         let mut best_state = init_state;
         let mut best_score = best_state.calc_score();
-        let mut rng: rand::rngs::StdRng = rand::SeedableRng::seed_from_u64(self.config.random_seed);
+        let mut rng: rand::rngs::StdRng = rand::SeedableRng::seed_from_u64(self.random_seed);
 
         let calc_temp = |elapsed: f64| -> f64 {
-            self.config.start_temp
-                + (self.config.end_temp - self.config.start_temp) * elapsed
-                    / self.config.time_limit_millis as f64
+            self.start_temp
+                + (self.end_temp - self.start_temp) * elapsed / self.time_limit_millis as f64
         };
 
         let calc_prob = |best_score: u64, score: u64, temp: f64| -> f64 {
